b65fd8521898f0c62ddbee178b0a22664ff9e031
amm0theman <ammonriley.97@gmail.com>
2018-11-01 17:38:20 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace seesharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
